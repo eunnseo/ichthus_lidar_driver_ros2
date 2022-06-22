ad77592a4828473430a4307b6762050cc748f2d3
eunseo <eunseo.choi.d@gmail.com>
2022-06-22 22:02:31 +0900

    clean

--- src/lib/backend.cpp
+++ src/lib/backend.cpp
@@ -40,7 +40,7 @@ namespace ichthus_lidar_driver_ros2
     void InputCloud::addCloud(pcl::PointCloud<PointT> &in_cloud)
     {
       // TODO: Merge deblurringPointCloud and transformPointCloud
-      // deblurringPointCloud(in_cloud);
+      deblurringPointCloud(in_cloud);
 
       pcl::PointCloud<PointT> tf_cloud;
       pcl::transformPointCloud(in_cloud, tf_cloud, transform_);
@@ -51,7 +51,6 @@ namespace ichthus_lidar_driver_ros2
 
     void InputCloud::popCloud(pcl::PointCloud<PointT> &out_cloud)
     {
-      // std::cout << "popCloud..\n";
       if (tf_cloud_.empty())
       {
         // std::cerr << "tf_cloud_ is empty." << std::endl;
@@ -76,7 +75,7 @@ namespace ichthus_lidar_driver_ros2
       //   static int seq1 = 0;
       //   pcl::io::savePCDFileBinary("/home/eunseo/tmp/not_deblurred/" + std::to_string(seq1++) + ".pcd", tf_cloud_);
       // }
-      deblurringPointCloud(tf_cloud_);
+      // deblurringPointCloud(tf_cloud_);
 
       // if (tf_cloud_.size() > 0)
       // {
@@ -94,25 +93,6 @@ namespace ichthus_lidar_driver_ros2
 
       while (!velocity_queue_.empty())
       {
-        // // for replay rosbag
-        // if (
-        //     rclcpp::Time(velocity_queue_.front().header.stamp) >
-        //     rclcpp::Time(vel.header.stamp))
-        // {
-        //   velocity_queue_.pop_front();
-        // }
-        // else if ( // NOLINT
-        //     rclcpp::Time(velocity_queue_.front().header.stamp) <
-        //     rclcpp::Time(vel.header.stamp) - rclcpp::Duration::from_seconds(1.0))
-        // {
-        //   velocity_queue_.pop_front();
-        // }
-        // else
-        // {
-        //   /* do nothing */
-        // }
-        // break; // ?
-
         if ( // NOLINT
             rclcpp::Time(velocity_queue_.front().header.stamp) <
             rclcpp::Time(vel.header.stamp) - rclcpp::Duration::from_seconds(1.0))
@@ -123,12 +103,9 @@ namespace ichthus_lidar_driver_ros2
         {
           /* do nothing */
         }
-        break; // ? break 안하면 while문을 못빠져나옴. 왜?
+        break;
       }
     }
-
-
-    ////////////////////////////// deblurring //////////////////////////////
 
     bool InputCloud::deblurringPointCloud(pcl::PointCloud<PointT> &cloud)
     {
@@ -145,9 +122,6 @@ namespace ichthus_lidar_driver_ros2
       const double last_point_time_stamp_sec{point_it->timestamp};
       double next_time_stamp_sec{point_it->timestamp};
 
-// std::cout << std::setprecision(30);
-// std::cout << "last_point_time_stamp_sec: " << last_point_time_stamp_sec << std::endl;
-
       auto velocity_it = std::lower_bound(
           std::begin(velocity_queue_), std::end(velocity_queue_),
           last_point_time_stamp_sec,
@@ -159,8 +133,6 @@ namespace ichthus_lidar_driver_ros2
                         ? std::end(velocity_queue_) - 1
                         : velocity_it;
 
-      // std::cout << "velocity_it->header.stamp = " << rclcpp::Time(velocity_it->header.stamp).seconds() << std::endl;
-
       const tf2::Transform tf2_base_link_to_sensor_inv{tf2_base_link_to_sensor_.inverse()};
     
       for (; point_it != cloud.begin(); --point_it)
@@ -170,8 +142,6 @@ namespace ichthus_lidar_driver_ros2
               point_it->timestamp > rclcpp::Time(velocity_it->header.stamp).seconds());
              ++velocity_it)
         {
-          // std::cout << "velocity_it->linear_x = " << velocity_it->linear_x << std::endl;
-          // std::cout << "velocity_it->angular_z = " << velocity_it->angular_z << std::endl;
         }
 
         float v{static_cast<float>(velocity_it->linear_x)};
@@ -180,9 +150,6 @@ namespace ichthus_lidar_driver_ros2
         double time_diff = std::abs(point_it->timestamp - rclcpp::Time(velocity_it->header.stamp).seconds());
         if (time_diff > 0.1)
         {
-          // std::cout << "point_it->timestamp: " << point_it->timestamp <<std::endl;
-          // std::cout << "velocity_it->header.stamp: " << rclcpp::Time(velocity_it->header.stamp).seconds() << std::endl;
-          // std::cout << "Velocity time_stamp is too late. (" <<  time_diff << ") " << "Cloud not interpolate." << std::endl;
           v = 0.0f;
           w = 0.0f;
         }
@@ -190,8 +157,6 @@ namespace ichthus_lidar_driver_ros2
         const double time_offset = static_cast<double>(next_time_stamp_sec - point_it->timestamp);
         if (time_offset < 0)
         {
-          std::cout << "Time offset < 0" << std::endl;
-          std::cout << "Time offset < 0" << std::endl;
           std::cout << "Time offset < 0" << std::endl;
         }
 
@@ -223,203 +188,6 @@ namespace ichthus_lidar_driver_ros2
       }
       return true;
     }
-
-
-    // Origin deblurring code
-    // bool InputCloud::deblurringPointCloud(pcl::PointCloud<PointT> &cloud)
-    // {
-    //   if (cloud.empty() || velocity_queue_.empty())
-    //   {
-    //     // std::cerr << "cloud or velocity_queue_ is empty." << std::endl;
-    //     return false;
-    //   }
-
-    //   pcl::PointCloud<PointT>::iterator point_it = cloud.begin();
-    //   float theta{0.0f};
-    //   float x{0.0f};
-    //   float y{0.0f};
-    //   double prev_time_stamp_sec{point_it->timestamp};
-    //   const double first_point_time_stamp_sec{point_it->timestamp};
-
-    //   auto velocity_it = std::lower_bound(
-    //       std::begin(velocity_queue_), std::end(velocity_queue_),
-    //       first_point_time_stamp_sec,
-    //       [](const sensor::Velocity &vel, const double t)
-    //       {
-    //         return rclcpp::Time(vel.header.stamp).seconds() < t;
-    //       });
-    //   velocity_it = velocity_it == std::end(velocity_queue_)
-    //                     ? std::end(velocity_queue_) - 1
-    //                     : velocity_it;
-
-    //   const tf2::Transform tf2_base_link_to_sensor_inv{tf2_base_link_to_sensor_.inverse()};
-    //   for (; point_it != cloud.end(); ++point_it)
-    //   {
-    //     for (;
-    //          (velocity_it != std::end(velocity_queue_) - 1 &&
-    //           point_it->timestamp > rclcpp::Time(velocity_it->header.stamp).seconds());
-    //          ++velocity_it)
-    //     {
-    //       // std::cout << "velocity_it->linear_x = " << velocity_it->linear_x << std::endl;
-    //       // std::cout << "velocity_it->angular_z = " << velocity_it->angular_z << std::endl;
-    //     }
-
-    //     float v{static_cast<float>(velocity_it->linear_x)};
-    //     float w{static_cast<float>(velocity_it->angular_z)};
-
-    //     double time_diff = std::abs(point_it->timestamp - rclcpp::Time(velocity_it->header.stamp).seconds());
-    //     if (time_diff > 0.1)
-    //     {
-    //       std::cout << "Velocity time_stamp is too late. (" <<  time_diff << ") " << "Cloud not interpolate." << std::endl;
-    //       v = 0.0f;
-    //       w = 0.0f;
-    //     }
-    //     // std::cout << "v = " << v << std::endl;
-    //     // std::cout << "w = " << w << std::endl;
-    //     // std::cout << "\n\n\n\n\n";
-
-    //     const double time_offset = static_cast<double>(point_it->timestamp - prev_time_stamp_sec);
-
-    //     const tf2::Vector3 sensorTF_point{point_it->x, point_it->y, point_it->z};
-
-    //     const tf2::Vector3 base_linkTF_point{tf2_base_link_to_sensor_inv * sensorTF_point};
-
-    //     theta += w * time_offset;
-    //     tf2::Quaternion baselink_quat{};
-    //     baselink_quat.setRPY(0.0, 0.0, theta);
-
-    //     const double dis = v * time_offset;
-    //     x += dis * std::cos(theta);
-    //     y += dis * std::sin(theta);
-
-    //     tf2::Transform baselinkTF_odom{};
-    //     baselinkTF_odom.setOrigin(tf2::Vector3(x, y, 0.0));
-    //     baselinkTF_odom.setRotation(baselink_quat);
-
-    //     const tf2::Vector3 base_linkTF_trans_point{baselinkTF_odom * base_linkTF_point};
-
-    //     const tf2::Vector3 sensorTF_trans_point{tf2_base_link_to_sensor_ * base_linkTF_trans_point};
-
-    //     point_it->x = sensorTF_trans_point.getX();
-    //     point_it->y = sensorTF_trans_point.getY();
-    //     point_it->z = sensorTF_trans_point.getZ();
-
-    //     prev_time_stamp_sec = point_it->timestamp;
-    //   }
-    //   return true;
-    // }
-
-//     bool InputCloud::deblurringPointCloudMinus(pcl::PointCloud<PointT> &cloud)
-//     {
-//       if (cloud.empty() || velocity_queue_.empty())
-//       {
-//         // std::cerr << "cloud or velocity_queue_ is empty." << std::endl;
-//         return false;
-//       }
-
-//       pcl::PointCloud<PointT>::iterator point_it = cloud.end() - 1;
-//       float theta{0.0f};
-//       float x{0.0f};
-//       float y{0.0f};
-//       const double last_point_time_stamp_sec{point_it->timestamp};
-//       double prev_time_stamp_sec{point_it->timestamp};
-
-// std::cout << std::setprecision(30);
-// // std::cout << "last_point_time_stamp_sec: " << last_point_time_stamp_sec << std::endl;
-      
-//       auto velocity_it = std::lower_bound(
-//           std::begin(velocity_queue_), std::end(velocity_queue_),
-//           last_point_time_stamp_sec,
-//           [](const sensor::Velocity &vel, const double t)
-//           {
-//             return rclcpp::Time(vel.header.stamp).seconds() < t;
-//           });
-//       velocity_it = velocity_it == std::end(velocity_queue_)
-//                         ? std::end(velocity_queue_) - 1
-//                         : velocity_it;
-
-//       // std::cout << std::setprecision(30);
-//       // std::cout << "velocity_it->header.stamp = " << rclcpp::Time(velocity_it->header.stamp).seconds() << std::endl;
-
-//       const tf2::Transform tf2_base_link_to_sensor_inv{tf2_base_link_to_sensor_.inverse()};
-
-//       --point_it;
-//       // for (; point_it != cloud.begin(); ++point_it)
-//       while (true)
-//       {
-//         // std::cout << "\n\nstart\n";
-//         // std::cout <<"point timestamp: "<< point_it->timestamp <<std::endl;
-//         for (velocity_it = velocity_queue_.begin(); 
-//             (velocity_it != velocity_queue_.end() - 1 && 
-//             point_it->timestamp > rclcpp::Time(velocity_it->header.stamp).seconds());
-//             ++velocity_it)
-//         {
-//           // std::cout << "velocity timestamp: " << rclcpp::Time(velocity_it->header.stamp).seconds() <<std::endl;
-//           // std::cout << "velocity_it->linear_x = " << velocity_it->linear_x << std::endl;
-//           // std::cout << "velocity_it->angular_z = " << velocity_it->angular_z << std::endl;
-//           ;
-//         }
-//         // std::cout << "end\n";
-
-//         float v{static_cast<float>(velocity_it->linear_x)};
-//         float w{static_cast<float>(velocity_it->angular_z)};
-
-//         double time_diff = std::abs(point_it->timestamp - rclcpp::Time(velocity_it->header.stamp).seconds());
-//         if (time_diff > 0.1)
-//         {
-//           // std::cout << "point_it->timestamp: " << point_it->timestamp <<std::endl;
-//           // std::cout <<"velocity_it->header.stamp: "<< rclcpp::Time(velocity_it->header.stamp).seconds() << std::endl;
-//           // std::cout << "Velocity time_stamp is too late. (" <<  time_diff << ") " << "Cloud not interpolate." << std::endl;
-//           v = 0.0f;
-//           w = 0.0f;
-//         }
-//         // std::cout << "v = " << v << std::endl;
-//         // std::cout << "w = " << w << std::endl;
-//         // std::cout << "\n\n\n\n\n";
-
-//         const double time_offset = static_cast<double>(prev_time_stamp_sec - point_it->timestamp);
-//         if (time_offset < 0)
-//         {
-//           std::cout << "Time offset < 0" << std::endl;
-//           std::cout << "Time offset < 0" << std::endl;
-//           std::cout << "Time offset < 0" << std::endl;
-//         }
-
-//         const tf2::Vector3 sensorTF_point{point_it->x, point_it->y, point_it->z};
-
-//         const tf2::Vector3 base_linkTF_point{tf2_base_link_to_sensor_inv * sensorTF_point};
-
-//         theta -= w * time_offset;
-//         tf2::Quaternion baselink_quat{};
-//         baselink_quat.setRPY(0.0, 0.0, theta);
-
-//         const double dis = v * time_offset;
-//         x -= dis * std::cos(theta);
-//         y -= dis * std::sin(theta);
-
-//         tf2::Transform baselinkTF_odom{};
-//         baselinkTF_odom.setOrigin(tf2::Vector3(x, y, 0.0));
-//         baselinkTF_odom.setRotation(baselink_quat);
-
-//         const tf2::Vector3 base_linkTF_trans_point{baselinkTF_odom * base_linkTF_point};
-
-//         const tf2::Vector3 sensorTF_trans_point{tf2_base_link_to_sensor_ * base_linkTF_trans_point};
-
-//         point_it->x = sensorTF_trans_point.getX();
-//         point_it->y = sensorTF_trans_point.getY();
-//         point_it->z = sensorTF_trans_point.getZ();
-//         prev_time_stamp_sec = point_it->timestamp;
-//         if (point_it == cloud.begin())
-//         {
-//           break;
-//         }
-//         else
-//         {
-//           --point_it;
-//         }
-//       }
-//       return true;
-//     }
 
   }
 }
